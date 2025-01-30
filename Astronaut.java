e6053e80f9adeb8a0910d6bb9fe91b1b741abfa8
Buster <busterbussin@gmail.com>
2025-01-30 13:18:14 -0500

    Numerous changes
    Started main and astronaut

--- Astronaut.java
+++ Astronaut.java
@@ -1,3 +1,35 @@
 public class Astronaut {
-    
+    String name;
+    String dob;
+    int serial;
+    String address;
+    String email;
+    int number;
+    double rate;
+    double weight;
+    String kin;
+    boolean status;
+    String phoneNum;
+
+    public Astronaut(String name, String dob, int serial, String address, String email, int number, double rate,
+            double weight, String kin, boolean status) {
+        this.name = name;
+        this.dob = dob;
+        this.serial = serial;
+        this.address = address;
+        this.email = email;
+        this.number = number;
+        this.rate = rate;
+        this.weight = weight;
+        this.kin = kin;
+        this.status = status;
+        phoneNum = String.format("(%03d)%03d-%04d",
+                number / 10000000,
+                (number / 10000) % 1000,
+                number % 10000);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- Main.java
+++ Main.java
@@ -1,9 +1,35 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
 public class Main {
     public void main(String args[]) {
-        
+        String password = "greenbeaneatingmachine";
+        String inputPassword = "a";
+        char option = 'A';
+        int numAstro = 0;
+        Scanner scan = new Scanner(System.in);
+        while(!inputPassword.equals(password)) {
+            System.out.println("Please enter the password.");
+            inputPassword = scan.nextLine();
+            if (!inputPassword.equals(password)) {
+                System.out.println("That's the wrong password.");
+            }
+        }
+        while(option != 'Z') {
+        System.out.println("Please select an option");
+        System.out.println("A) Edit Astronauts");
+        System.out.println("Z) Exit");
+        option = scan.nextLine().toUpperCase().charAt(0);
+        while(option == 'A') {
+            if (numAstro == 0) {
+                System.out.println("Please enter the number of astronauts");
+                numAstro = scan.nextInt();
+                while (numAstro <= 0) {
+                    System.out.println("Number must be above 0. Please enter a valid number.");
+                    numAstro = scan.nextInt();
+                }
+                ArrayList<Astronaut> astros = new ArrayList<>();
+            }
+        }
+        }
     }
 }
-
-//
