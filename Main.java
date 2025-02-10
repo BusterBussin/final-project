ad36a8a5f20ac2ef922a42f7fb41573dbd7cdd4b
Buster <busterbussin@gmail.com>
2025-02-10 12:51:44 -0500

    sqlite

--- Main.java
+++ Main.java
@@ -1,8 +1,22 @@
 import java.util.Scanner;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Main {
+
+    public static void connect() {
+        // connection string
+        var url = "jdbc:sqlite:c:/sqlite/db/chinook.db";
+
+        try (var conn = DriverManager.getConnection(url)) {
+            System.out.println("Connection to SQLite has been established.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
     public static void main(String args[]) {
+        connect();
         String password = "greenbeaneatingmachine";
         String inputPassword = "a";
         char option = 'A';
